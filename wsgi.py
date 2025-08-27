f7da185ef0cdf9f568ad365d3fe56df8a300970a
Esteban Olaya <juanesteban199911@gmail.com>
2025-08-27 03:28:54 -0500

    feat(server): add WSGI entrypoint and Uvicorn reload run script

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from src.presentation.fastapi_app import create_app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+    # Use import string for reload support
+    uvicorn.run("wsgi:app", host="0.0.0.0", port=8000, reload=True)
+
+
